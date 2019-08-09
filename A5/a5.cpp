b90bd7d5079b2832a47170e19dbe2b2dd0ae8fde
Isaac Rezey <isaacrez@buffalo.edu>
2019-08-08 22:28:09 -0400

    Initial commit

---
+++ A5/a5.cpp
@@ -0,0 +1,19 @@
+// File: a5.cpp
+// Isaac
+// Rezey
+// I AFFIRM THAT WHEN WORKING ON THIS ASSIGNMENT
+// I WILL FOLLOW UB STUDENT CODE OF CONDUCT, AND
+// I WILL NOT VIOLATE ACADEMIC INTEGRITY RULES
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+
+	if (argc != 2){
+		std::cout << "Incorrect inputs.  Use as ./a5 filename" << std::endl;
+	}
+
+	// Read the dictionary
+}
+
